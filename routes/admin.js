eca657fdd406e1bf1aa090fded15630f4b583aab
Mark Leo <ratrateroo@gmail.com>
2020-02-04 03:53:11 +0800

    added helper functions

--- routes/admin.js
+++ routes/admin.js
@@ -2,16 +2,18 @@ const path = require('path');
 
 const express = require('express');
 
+const rootDir = require('../util/path');
+
 const router = express.Router();
 
 router.get('/add-movies', (req, res, next) => {
-    res.sendFile(path.join(__dirname, '../', 'views', 'add-movies.html'));
+    res.sendFile(path.join(rootDir, 'views', 'add-movies.html'));
   });
   
 router.post('/movie', (req, res, next) => {
       console.log(req.body);
       //res.send('<h1>Movie '+ req.body.title +' Page</h1>');
-      res.redirect('/movie');
+      res.redirect('/');
   });
 
 module.exports = router;
--- routes/movies.js
+++ routes/movies.js
@@ -2,10 +2,12 @@ const path = require('path');
 
 const express = require('express');
 
+const rootDir = require('../util/path');
+
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-    res.sendFile(path.join(__dirname, '../', 'views', 'movies.html'));
+    res.sendFile(path.join(rootDir, 'views', 'movies.html'));
 });
 
 module.exports = router;
