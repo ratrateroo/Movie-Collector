a10d8defa5c7a80a2034f458489695d92db48e7e
Mark Leo <ratrateroo@gmail.com>
2020-02-04 02:02:25 +0800

    made some adjustments

--- app.js
+++ app.js
@@ -5,15 +5,9 @@ const express = require('express');
 const app = express();
 
 app.use((req, res, next) => {
-    console.log('Middleware');
-    next();
-});
-
-app.use((req, res, next) => {
     console.log('Middleware Again');
     res.send('<h1>Middleware Again</h1>');
 });
 
-const server = http.createServer(app);
 
-server.listen(3000);
+app.listen(3000);
