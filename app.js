e429becd613e585f1c52abb137396b94e76b5ab4
Mark Leo <ratrateroo@gmail.com>
2020-02-04 01:53:45 +0800

    tested out express

--- app.js
+++ app.js
@@ -2,6 +2,18 @@ const http = require('http');
 
 const express = require('express');
 
-const server = http.createServer();
+const app = express();
+
+app.use((req, res, next) => {
+    console.log('Middleware');
+    next();
+});
+
+app.use((req, res, next) => {
+    console.log('Middleware Again');
+    res.send('<h1>Middleware Again</h1>');
+});
+
+const server = http.createServer(app);
 
 server.listen(3000);
