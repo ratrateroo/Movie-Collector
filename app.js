c64dab29cdcba8a664749d79b19efda8bc3c31b9
Mark Leo <ratrateroo@gmail.com>
2020-02-04 03:43:05 +0800

    added a 404 view

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const http = require('http');
+const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
@@ -16,7 +16,7 @@ app.use('/admin', adminRoutes);
 app.use(moviesRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).send('<h1> Page not found!</h1>')
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 }) ;
 
 app.listen(3000);
