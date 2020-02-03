14d5953d23408195ffa928d6c110649c6080985c
Mark Leo <ratrateroo@gmail.com>
2020-02-04 03:12:30 +0800

    added a 404 page

--- app.js
+++ app.js
@@ -15,14 +15,8 @@ app.use(bodyParser.urlencoded({
 app.use(adminRoutes);
 app.use(moviesRoutes);
 
-
-
-
-
-
-
-
-
-
+app.use((req, res, next) => {
+    res.status(404).send('<h1> Page not found!</h1>')
+}) ;
 
 app.listen(3000);
