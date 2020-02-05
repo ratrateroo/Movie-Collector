2130699d19120ccf699cdbaa67860224e47990bf
Mark Leo <ratrateroo@gmail.com>
2020-02-05 19:31:32 +0800

    added handlebars as new templating engine

--- app.js
+++ app.js
@@ -2,10 +2,13 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const expressHbs = require('express-handlebars');
+
 
 const app = express();
 
-app.set('view engine', 'pug');
+app.engine('handlebars', expressHbs());
+app.set('view engine', 'handlebars');
 app.set('views', 'views');
 
 const adminData = require('./routes/admin');
@@ -20,7 +23,8 @@ app.use('/admin', adminData.routes);
 app.use(moviesRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+    //res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+    res.status(404).render('404');
 }) ;
 
 app.listen(3000);
