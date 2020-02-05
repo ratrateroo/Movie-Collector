f407f49bf026116a98c3ab1bc6e5362f132656fc
Mark Leo <ratrateroo@gmail.com>
2020-02-06 03:12:59 +0800

    added some refactoring

--- models/movie.js
+++ models/movie.js
@@ -1,6 +1,22 @@
 //const movies = [];
 const fs = require('fs');
 const path = require('path');
+
+const getMoviesFromFile = cb => {
+    const m = path.join(
+        path.dirname(process.mainModule.filename),
+        'data',
+        'movies.json'
+        );
+    fs.readFile(m, (error, fileContent) => {
+        if (error) {
+         cb([]);
+        } else {
+         cb(JSON.parse(fileContent));
+        }
+    });
+};
+
 module.exports = class Product {
     constructor(title, year) {
         this.title = title;
@@ -8,36 +24,15 @@ module.exports = class Product {
     }
 
     save() {
-        //movies.push(this);
-        const m = path.join(
-            path.dirname(process.mainModule.filename),
-            'data',
-            'movies.json'
-            );
-        fs.readFile(m, (error, fileContent) => {
-            let movies = [];
-            if (!error) {
-                movies = JSON.parse(fileContent);
-            }
+        getMoviesFromFile(movies => {
             movies.push(this);
             fs.writeFile(m, JSON.stringify(movies), (error) => {
                 console.log(error);
-            })
-        })
+            });            
+        });        
     }
 
     static fetchAll(cb) {
-        const m = path.join(
-            path.dirname(process.mainModule.filename),
-            'data',
-            'movies.json'
-            );
-        fs.readFile(m, (error, fileContent) => {
-            if (error) {
-                cb([]);
-            }
-            cb(JSON.parse(fileContent));
-        });
-    
+        getMoviesFromFile(cb);
     }
 }
