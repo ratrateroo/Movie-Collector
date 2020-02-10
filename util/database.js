313fed0840d3149afee0fe2fc9542afa3a356a01
Mark Leo <ratrateroo@gmail.com>
2020-02-11 02:34:56 +0800

    connected to mongodb

--- util/database.js
+++ util/database.js
@@ -1,3 +1,15 @@
 const mongodb = require('mongodb');
 
 const MongoClient = mongodb.MongoClient;
+
+MongoClient.connect(
+    'mongodb+srv://ratrateroo:UltraPassword@moviecollector-icuyt.mongodb.net/test?retryWrites=true&w=majority'
+    )
+    .then(result => {
+        console.log('Connected');
+    })
+    .catch(error => {
+        console.log(error);
+    });
+
+
