e2d67e72f9e2be0c95c753e7835fb533a7f0631b
Mark Leo <ratrateroo@gmail.com>
2020-02-19 23:21:28 +0800

    fixing some bugs

--- controllers/admin.js
+++ controllers/admin.js
@@ -44,6 +44,7 @@ exports.postAddToCollection = (req, res, next) => {
   fetch(endpoint)
             .then(movie => movie.json())
             .then(result => {
+              console.log(result);
               const movie = new Movie({
                 title: result.title,
                 year: result.release_date,
@@ -62,47 +63,47 @@ exports.postAddToCollection = (req, res, next) => {
           }); 
 
 
-  const title = req.body.title;
-  const year = req.body.year;
-  const imageUrl = req.body.imageUrl;
-  //const movie = new Movie(title, year, imageUrl, null, req.user._id);
-  const movie = new Movie({
-      title: title,
-      year: year,
-      imageUrl: "../badboysforlife.jpg",
-      userId: req.user
-  });
-  movie.save()
-  .then(result => {
-      console.log('Post Added Movie:' + result);
-      res.redirect('my-movies');  
-  })
-  .catch(error => {
-      console.log('Post Add Movie Error: ' + error);
-  }); 
+  // const title = req.body.title;
+  // const year = req.body.year;
+  // const imageUrl = req.body.imageUrl;
+  // //const movie = new Movie(title, year, imageUrl, null, req.user._id);
+  // const movie = new Movie({
+  //     title: title,
+  //     year: year,
+  //     imageUrl: "../badboysforlife.jpg",
+  //     userId: req.user
+  // });
+  // movie.save()
+  // .then(result => {
+  //     console.log('Post Added Movie:' + result);
+  //     res.redirect('my-movies');  
+  // })
+  // .catch(error => {
+  //     console.log('Post Add Movie Error: ' + error);
+  // }); 
 
 
     
-    fetch(endpoint)
-            .then(movie => movie.json())
+  //   fetch(endpoint)
+  //           .then(movie => movie.json())
              
-            .then(movie => {
-                console.log(movie);
-                console.log(IMAGE_BASE_URL);
-                console.log(IMAGE_SIZE);
-                console.log(POSTER_SIZE);
-                res.render('movies/movie', { 
-                    movie: movie,
-                    pageTitle: movie.title,
-                    imageBaseUrl: IMAGE_BASE_URL,
-                    imageSize: IMAGE_SIZE,
-                    posterSize: POSTER_SIZE,    
-                    path: 'movies/movies',
-                    activeMovies: true,
-                    isAuthenticated: req.session.isLoggedIn
-                });
-            })
-            .catch(error => console.log(error));
+  //           .then(movie => {
+  //               console.log(movie);
+  //               console.log(IMAGE_BASE_URL);
+  //               console.log(IMAGE_SIZE);
+  //               console.log(POSTER_SIZE);
+  //               res.render('movies/movie', { 
+  //                   movie: movie,
+  //                   pageTitle: movie.title,
+  //                   imageBaseUrl: IMAGE_BASE_URL,
+  //                   imageSize: IMAGE_SIZE,
+  //                   posterSize: POSTER_SIZE,    
+  //                   path: 'movies/movies',
+  //                   activeMovies: true,
+  //                   isAuthenticated: req.session.isLoggedIn
+  //               });
+  //           })
+  //           .catch(error => console.log(error));
      
 };
 
