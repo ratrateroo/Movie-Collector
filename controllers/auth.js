d4eb4276dedf2e3a1dafae77a544ebba08523b56
Mark <ratrateroo@gmail.com>
2020-02-20 23:59:21 +0800

    added update password feature

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,5 @@
+const crypto = require('crypto');
 const bcrypt = require('bcryptjs');
-
 const User = require('../models/user');
 
 exports.getLogin = (req, res, next) => {
@@ -128,7 +128,59 @@ exports.getSignup = (req, res, next) => {
     res.render('auth/reset', {
         path: 'admin/reset',
         pageTitle: 'Reset Password',
-        isAuthenticated: false,
         errorMessage: message
       });
   }
+
+  exports.postReset = (req, res, next) => {
+      crypto.randomBytes(32, (err, buffer) => {
+          if (err) {
+              console.log(err);
+              return res.redirect('/reset');
+          }
+          const token = buffer.toString('hex');
+          User.findOne({email: req.body.email})
+                .then(user => {
+                    if (!user) {
+                        req.flash('error', 'No account with that email found.');
+                        return res.redirect('/reset');
+                    }
+                    user.resetToken = token;
+                    user.resetTokenExpiration = Date.now() + 3600000;
+                    return user.save();
+                })
+                .then(result => {
+                    return res.redirect('/reset/' + token);
+                })
+                .catch(err => console.log(err));
+      })
+  }
+
+exports.getNewPassword = (req, res, next) => {
+    const token = req.params.token;
+    User.findOne({resetToken: token, resetTokenExpiration: {$gt: Date.now()}})
+    .then(user => {
+        let message = req.flash('error');
+        if (message.length > 0) {
+            message = message[0];
+        } else {
+        message = null;
+        }
+        res.render('auth/new-password', {
+            path: '/new-password',
+            pageTitle: 'Update Password',            
+            errorMessage: message,
+            userId: user._id.toString()
+        });
+    })
+    .catch(err => console.log(err));
+
+    let message = req.flash('error');
+    if (message.length > 0) {
+        message = message[0];
+    } else {
+        message = null;
+    }
+    
+      
+}
--- routes/auth.js
+++ routes/auth.js
@@ -10,6 +10,8 @@ router.get('/signup', authController.getSignup);
 router.post('/signup', authController.postSignup);
 router.post('/logout', authController.postLogout);
 router.get('/reset', authController.getReset);
+router.post('/reset', authController.postReset);
+router.get('/reset/:token', authController.getNewPassword);
 
 
 module.exports = router;
