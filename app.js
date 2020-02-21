8dc10d8cb6cb4e7df7a6775f0b0ac5b310079688
Mark <ratrateroo@gmail.com>
2020-02-21 23:39:55 +0800

    served images statically

--- app.js
+++ app.js
@@ -68,6 +68,8 @@ app.use(
     multer( { storage: fileStorage, fileFilter: fileFilter }).single('image')
   );
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/images', express.static(path.join(__dirname, 'images')));
+
 app.use(session({
     secret: 'secret',
     resave: false,
