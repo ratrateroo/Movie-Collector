498c1b85c4ec4cbb297bc5f447ec9740bed4b701
Mark <ratrateroo@gmail.com>
2020-02-22 04:26:54 +0800

    added codes for creating elements

--- public/js/load.js
+++ public/js/load.js
@@ -1,3 +1,4 @@
+let pageNumber = 1;
 const URL =
 "https://gist.githubusercontent.com/prof3ssorSt3v3/1944e7ba7ffb62fe771c51764f7977a4/raw/c58a342ab149fbbb9bb19c94e278d64702833270/infinite.json";
 document.addEventListener("DOMContentLoaded", () => {
@@ -8,34 +9,51 @@ let options = {
   rootMargins: "0px",
   threshold: 0.5
 };
+
 const observer = new IntersectionObserver(handleIntersect, options);
-observer.observe(document.querySelector("footer"));
+observer.observe(document.querySelector(".all-rights-reserved"));
 //an initial load of some data
-getData();
+//getData();
+getMovies(pageNumber);
 });
+
+function getMovies(pageNumber) {
+    let theurl = 'http://localhost:3000/load-movies/' + pageNumber;
+    fetch(theurl)
+    .then(response => response.json())
+    .then(result => {
+        console.log(result.results);
+    })
+    .catch(err => console.log(err));
+}
+
+
 function handleIntersect(entries) {
 if (entries[0].isIntersecting) {
   console.warn("something is intersecting with the viewport");
-  getData();
+  pageNumber++;
+  getMovies(pageNumber);
+  //getData();
 }
 }
-function getData() {
-let main = document.querySelector("main");
-console.log("fetch some JSON data");
-fetch(URL)
-  .then(response => response.json())
-  .then(data => {
-    // data.items[].img, data.items[].name
-    data.items.forEach(item => {
-      let fig = document.createElement("figure");
-      let fc = document.createElement("figcaption");
-      let img = document.createElement("img");
-      img.src = item.img;
-      img.alt = item.name;
-      fc.textContent = item.name;
-      fig.appendChild(img);
-      fig.appendChild(fc);
-      main.appendChild(fig);
-    });
-  });
-}
+const mainArea = document.querySelector(".main-area");
+
+let movie = document.createElement("div");
+movie.setAttribute("class", "movie");
+
+let movieHeader = document.createElement("div"); 
+movieHeader.setAttribute("class", "movieHeader");
+
+let movieTitle = document.createElement("div");
+movieTitle.setAttribute("class", "movieTitle");
+
+let h3 = document.createElement("h3");
+let text = document.createTextNode("Movie");
+h3.appendChild(text);
+
+movieTitle.appendChild(h3);
+movieHeader.appendChild(movieTitle);
+movie.appendChild(movieHeader);
+
+
+mainArea.appendChild(movie);
